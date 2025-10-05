b2f550f16d47f6502a7e5f0c48d6e7063959343d
BitcoinManor <62268768+BitcoinManor@users.noreply.github.com>
2025-10-05 03:13:28 -0600

    Create initial AxeStack Spark firmware scaffold
    Initial scaffold for AxeStack Spark firmware with setup and loop functions.

---
+++ firmware/AxeStack_Spark.ino
@@ -0,0 +1,11 @@
+// AxeStack — Spark (ESP32-S3) firmware
+// v0.1 scaffold: LVGL/display init will be added in next steps.
+
+void setup() {
+  // TODO: init display + LVGL
+  // TODO: show "AxeStack — scaffolding" label
+}
+
+void loop() {
+  // TODO: lv_timer_handler() and short delay
+}
